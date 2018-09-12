cb22d82f93900b89824b3da55bd7b2d6dd8a8415
Sorin Maftei <masodu@gmail.com>
2018-09-12 19:43:08 +0200

    react router

---
+++ src/index.js
@@ -0,0 +1,46 @@
+/**************************************************************************
+ *
+ *    ADOBE CONFIDENTIAL
+ *    ___________________
+ *
+ *    Copyright 2018 Adobe
+ *    All Rights Reserved.
+ *
+ *    NOTICE:  All information contained herein is, and remains
+ *    the property of Adobe and its suppliers, if any. The intellectual
+ *    and technical concepts contained herein are proprietary to Adobe
+ *    and its suppliers and are protected by all applicable intellectual
+ *    property laws, including trade secret and copyright laws.
+ *    Dissemination of this information or reproduction of this material
+ *    is strictly forbidden unless prior written permission is obtained
+ *    from Adobe.
+ *
+ **************************************************************************/
+
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import productDetailsReducer from './reducers/productDetails';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router';
+import { Route } from 'react-router'
+import ProductDetails from './components/ProductDetails/ProductDetails';
+
+const history = createBrowserHistory();
+// Initialize redux store
+const store = createStore(productDetailsReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    compose(applyMiddleware(thunk)));
+
+// Expose function to render ProductDetails component
+ReactDOM.render(   
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={ProductDetails} />
+        </Router>
+    </Provider>
+, document.getElementById("root"));
